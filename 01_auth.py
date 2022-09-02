82d352aa22209a0eeac08c111a3f541e5e284ab1
taoza <itbj00@163.com>
2022-09-03 02:51:34 +0800

    Update 01_auth.py

--- 01_auth.py
+++ 01_auth.py
@@ -9,7 +9,7 @@ def get_auth_token():
     """
     url = 'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token'                    # Endpoint URL
     hdr = {'content-type' : 'application/json'}                                           # Define request header
-    resp = requests.post(url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASSWORD), headers=hdr)  # Make the POST Request
+    resp = requests.post(url, auth=HTTPBasicAuth(DNAC_USER, DNAC_PASSWORD), headers=hdr, verify=False)  # Make the POST Request 20220903T0240am
     token = resp.json()['Token']                                                          # Retrieve the Token
     print("Token Retrieved: {}".format(token))                                            # Print out the Token
     return token    # Create a return statement to send the token back for later use
